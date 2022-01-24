9122a76c639a24f053d8bbfdaf52f0e5858a94f4
Austin Paolello <ajpaolello@gmail.com>
2022-01-24 15:29:16 -0500

    Added comments to methods as well as cleaned up some unneeded variables while also adding an exponent method

--- calculator.py
+++ calculator.py
@@ -9,58 +9,126 @@
 
 
 def add(num1, num2):
-    sum = num1 + num2
-    return sum
+    """
+    A simple function to calculate the sum of two numbers
+    :param num1: a numeric value
+    :param num2: another numeric value
+    :return: sum of num1 and num2
+    """
+    num1 += num2
+    return num1
 
 
 def subtract(num1, num2):
-    difference = num1 - num2
-    return difference
+    """
+    A simple function to calculate the difference of two numbers
+    :param num1: a numeric value
+    :param num2: another numeric value
+    :return: difference of num1 and num2
+    """
+    num1 -= num2
+    return num1
 
 
 def multiply(num1, num2):
-    product = num1 * num2
-    return product
+    """
+    A simple function to calculate the product of two numbers
+    :param num1: a numeric value
+    :param num2: another numeric value
+    :return: product of num1 and num2
+    """
+    num1 *= num2
+    return num1
 
 
 def divide(num1, num2):
-    quotient = num1 / num2
-    return quotient
+    """
+    A simple function to calculate the quotient of two numbers
+    :param num1: a numeric value
+    :param num2: another numeric value
+    :return: quotient of num1 and num2
+    """
+    num1 /= num2
+    return num1
 
 
 def divide_whole(num1, num2):
-    quotient = num1 // num2
-    return quotient
+    """
+    Similar to the divide function, but this returns a whole number.
+    Honestly not sure how useful this is, but I'm sure there is some weird use case
+    :param num1: a numeric value
+    :param num2: another numeric value
+    :return: quotient of num1 and num2
+    """
+    num1 //= num2
+    return num1
 
 
 def modulo(num1, num2):
-    remainder = num1 % num2
-    return remainder
+    """
+    A simple function to calculate the remainder of two numbers
+    :param num1: a numeric value
+    :param num2: another numeric value
+    :return: remainder of num1 and num2
+    """
+    num1 %= num2
+    return num1
+
+
+def exponent(num1, num2):
+    """
+    A simple function to calculate the exponent of two numbers
+    :param num1: a numeric value
+    :param num2: another numeric value
+    :return: exponent of num1 and num2
+    """
+    result = num1
+    if num2 == 0:
+        return 1
+    else:
+        for i in range(int(num2) - 1):
+            result *= num1
+        return result
 
 
 def main():
+    # Basic menu and instructions
     print("Welcome to the Python Calculator!")
     print("I can do addition, subtraction, multiplication, division, and modulo")
     print("At the moment I can only do two numbers, but that should change in the future")
     print("Please enter which operation you wish to do:")
-    print()
     print("1. Addition")
     print("2. Subtraction")
     print("3. Multiplication")
     print("4. Division")
     print("5. Division with whole numbers")
     print("6. Modulo")
-    print("7. Quit")
+    print("7. Exponent")
+    print("8. Quit")
     print()
     choice = 0
     num1 = 0
     num2 = 0
     result = 0
-    while choice < 1 or choice > 7:
+    while choice < 1 or choice > 8:
         choice = int(input("Please enter your choice: "))
         if 7 >= choice >= 1:
-            num1 = float(input("Please enter the first number: "))
-            num2 = float(input("Please enter the second number: "))
+            # Making sure user entered a number
+            while True:
+                num1 = input("Please enter the first number: ")
+                try:
+                    num1 = float(num1)
+                    break
+                except ValueError:
+                    print("Please enter a number!!")
+            # Checking the second number
+            while True:
+                num2 = input("Please enter the second number: ")
+                try:
+                    num2 = float(num2)
+                    break
+                except ValueError:
+                    print("Please enter a number!!")
         if choice == 1:
             result = add(num1, num2)
         elif choice == 2:
@@ -74,12 +142,19 @@ def main():
         elif choice == 6:
             result = modulo(num1, num2)
         elif choice == 7:
+            result = exponent(num1, num2)
+        elif choice == 8:
             break
         else:
             print("Please enter a valid choice!!")
 
-    if 1 <= choice <= 7:
-        print("The result is: ", result)
+        if 1 <= choice <= 7:
+            # Print result and ask to do another calculation
+            print("The result is: ", result)
+            answer = input("Would you like to perform another calculation? (y/n): ")
+            if answer == "y" or answer == "Y":
+                choice = 0
 
 
+# Runs main function
 main()
